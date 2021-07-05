5f3db6441fe5d0229bdb2296a696c11a0195189f
kuukie-kq <84168084+kuukie-kq@users.noreply.github.com>
2021-07-05 12:33:22 +0800

    Add files via upload

---
+++ HeapTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import java.util.ArrayList;
+
+public class HeapTest {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        while (true) {
+            list.add(new HeapTest());
+        }
+    }
+}
---
+++ StackTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.ArrayList;
+
+public class StackTest {
+    public void stack() {
+        stack();
+    }
+
+    public static void main(String[] args) {
+        new StackTest().stack();
+    }
+}
