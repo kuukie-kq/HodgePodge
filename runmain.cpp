bc32c43f73a23eb4c1d6599591f823c7c70d41b1
kuukie-kq <84168084+kuukie-kq@users.noreply.github.com>
2021-07-17 10:29:35 +0800

    Add files via upload

---
+++ runmain.cpp
@@ -0,0 +1,26 @@
+//
+// Created by polite on 2021/7/16.
+//
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <iostream>
+using namespace std;
+
+int mains() {
+    pid_t pid = fork();
+    if(pid < 0) {
+        cerr << "fork apply error\n";
+        return 0;
+    } else if (pid == 0) {
+        cout << "Sub-process PID:" << getpid() << " PPID:" << getppid() << "\n\n";
+        char* args[] = {"/home/polite/","test",(char*)0};
+        char* evnp[] = {0};
+        execve("/home/polite/test",args,evnp);
+    } else {
+        cout << "Parent process\n";
+        pid_t pr = wait(NULL);
+        cout << "\nParent process, I catched a child process with pid of " << pr << "\n";
+    }
+}
