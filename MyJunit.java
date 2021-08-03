ff00d9cd0babb9c3687a7f3d979ac522a5fff541
kuukie-kq <84168084+kuukie-kq@users.noreply.github.com>
2021-08-03 21:07:53 +0800

    Add files via upload

---
+++ MyJunit.java
@@ -0,0 +1,46 @@
+package com.java.main.self;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyJunit {
+	//传入一个对象实例
+    public static void run(Class c) throws InstantiationException,IllegalAccessException, IllegalArgumentException,InvocationTargetException {
+        Method[] methods = c.getDeclaredMethods();//获取对象声明的方法集合
+        List<Method> testList = new ArrayList<Method>();//@Mytest注解的方法集合
+        Method afterMethod = null;//@Myafter注解方法
+        Method beforeMethod = null;//@Mybefore的注解方法
+        for (Method method : methods) {//循环对象声明的所有方法
+        //如果有@Mytest注解，则将该方法加入到Mytest注解的方法集合
+            if (method.isAnnotationPresent(Mytest.class)) {
+                testList.add(method);
+            }
+            //如果有@Before注解，则引用该方法
+            if (method.isAnnotationPresent(Mybefore.class)) {
+                beforeMethod = method;
+            }
+            //如果有@After注解，则引用该方法
+            if (method.isAnnotationPresent(Myafter.class)) {
+                afterMethod = method;
+
+            }
+
+        }
+        //new一个对象实例
+        Object obj = c.newInstance();
+        //反射激活方法
+        for (Method m : testList) {
+            if (beforeMethod != null) {
+                beforeMethod.invoke(obj, null);
+            }
+
+            m.invoke(obj, null);
+            if (afterMethod != null) {
+                afterMethod.invoke(obj, null);
+            }
+
+        }
+    }
+}
---
+++ Myafter.java
@@ -0,0 +1,12 @@
+package com.java.main.self;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value=ElementType.METHOD)//作用域为方法
+@Retention(RetentionPolicy.RUNTIME)//注解信息保留到运行时
+public @interface Myafter {
+
+}
