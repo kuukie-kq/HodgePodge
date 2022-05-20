d123ce780adbd708288661ab6e1c140dfab16428
kuukie-kq <84168084+kuukie-kq@users.noreply.github.com>
2022-05-20 17:46:39 +0800

    Update interpreter.h

--- interpreter.h
+++ interpreter.h
@@ -8,8 +8,8 @@
 using namespace std;
 
 namespace cip {
-    //Created by kuu kie on 2022/4/30
-    //Content 目录 (line number) ~ (+40) ~ (line number) CLang-Tidy
+    //Created by kuu*kie on 2022/4/30
+    //Content 目录 (line number) ~ (+30) ~ (line number) CLang-Tidy
     //链接  目录末尾位置
     class [[maybe_unused]] tag_one;
     //类型定义
