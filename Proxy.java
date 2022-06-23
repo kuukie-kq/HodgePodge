3b9fbe66a417a6d83252622bafb34b4b150fcfaf
kuukie-kq <84168084+kuukie-kq@users.noreply.github.com>
2022-06-23 08:17:02 +0800

    Update Proxy.java

--- Proxy.java
+++ Proxy.java
@@ -11,7 +11,7 @@ import javax.tools.StandardJavaFileManager;
 import javax.tools.ToolProvider;
 import javax.tools.JavaCompiler.CompilationTask;
 
-//IOC AOP 核心部分 反射
+//IOC AOP 核心部分 反射 2021/5/20
 public class Proxy {
 	public static Object newProxyInstance(String str) throws Exception {
         //生成Java源文件  
