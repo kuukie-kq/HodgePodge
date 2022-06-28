bcec37874ed265afaac90ce943237ab5eb08b595
kuukie-kq <3260681415@qq.com>
2022-06-28 13:55:34 +0800

    7sCCCE

--- Ppocket/kuukie_exercise_db.sql
+++ Ppocket/kuukie_exercise_db.sql
@@ -5,7 +5,7 @@
  Source Server Type    : MySQL
  Source Server Version : 50711
  Source Host           : localhost:3306
- Source Schema         : virtual_router_db
+ Source Schema         : kuukie_exercise_db
 
  Target Server Type    : MySQL
  Target Server Version : 50711
