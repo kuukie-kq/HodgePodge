22831e8f746d7291970953303d30790af8d0d522
kuukie-kq <3260681415@qq.com>
2022-10-19 08:59:15 +0800

    7sCCDB

--- Spocket/kuu_custom_automatic_db.sql
+++ Spocket/kuu_custom_automatic_db.sql
@@ -1,34 +1,20 @@
-##Navicat Premium Data Transfer
-##
-## Source Server         : wsl_mysql_tmp
-## Source Server Type    : MySQL
-## Source Server Version : 80030
-## Source Host           : 172.16.192.2:3306
-## Source Schema         : kuu_custom_automatic_db
-##
-## Target Server Type    : MySQL
-## Target Server Version : 80030
-## File Encoding         : 65001
-##
-## Date: 2022-08-12 10:42:06
-
-##The start
-##This is added manually.
-## It is used to ensure the consistency of the library name.
+--The start
+--This is added manually.
+-- It is used to ensure the consistency of the library name.
 DROP DATABASE IF EXISTS `kuu_custom_automatic_db`;
 CREATE DATABASE `kuu_custom_automatic_db`;
 USE `kuu_custom_automatic_db`;
-##You are also use the next way
-# CREATE DATABASE IF NOT EXISTS `kuu_custom_automatic_db`;
-# USE `kuu_custom_automatic_db`;
-##The end
+--You are also use the next way
+-- CREATE DATABASE IF NOT EXISTS `kuu_custom_automatic_db`;
+-- USE `kuu_custom_automatic_db`;
+--The end
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
-## ----------------------------
-## Table structure for db_base_user
-## ----------------------------
+-- ----------------------------
+-- Table structure for db_base_user
+-- ----------------------------
 DROP TABLE IF EXISTS `db_base_user`;
 CREATE TABLE `db_base_user`  (
   `dbu_id` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
@@ -38,9 +24,9 @@ CREATE TABLE `db_base_user`  (
   PRIMARY KEY (`dbu_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-## ----------------------------
-## Table structure for db_base_menu
-## ----------------------------
+-- ----------------------------
+-- Table structure for db_base_menu
+-- ----------------------------
 DROP TABLE IF EXISTS `db_base_menu`;
 CREATE TABLE `db_base_menu`  (
   `dbm_reminder` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
@@ -49,9 +35,9 @@ CREATE TABLE `db_base_menu`  (
   PRIMARY KEY (`dbm_reminder`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-## ----------------------------
-## Table structure for db_base_view
-## ----------------------------
+-- ----------------------------
+-- Table structure for db_base_view
+-- ----------------------------
 DROP TABLE IF EXISTS `db_base_view`;
 CREATE TABLE `db_base_view`  (
   `dbv_reminder` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
