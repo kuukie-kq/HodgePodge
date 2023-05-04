f26fab1d60841bd72e5b183a922f72703b0ab3e0
kuukie-kq <3260681415@qq.com>
2023-05-04 17:21:48 +0800

    7sCCCC

--- Cclion/runmain.cpp
+++ Cclion/runmain.cpp
@@ -16,9 +16,9 @@ int main() {
         return 0;
     } else if (pid == 0) {
         cout << "Sub-process PID:" << getpid() << " PPID:" << getppid() << "\n\n";
-        char* args[] = {"/home/polite/","test",(char*)0};
+        char* args[] = {"/home/kuukie/","test",(char*)0};
         char* evnp[] = {0};
-        execve("/home/polite/test",args,evnp);
+        execve("/home/kuukie/test",args,evnp);
     } else {
         cout << "Parent process\n";
         pid_t pr = wait(NULL);
